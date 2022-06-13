74d6e8633eb45c6dd4347a746939e50ac2951487
Sebastian Gomez <sebasgomez1099@gmail.com>
2022-06-13 10:04:00 -0500

    se agraga la configuracion del puerto

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ const express = require('express');
  
 const config = require('./server/config');
 app = config(express());
+
+app.listen(app.get('port'), () => {
+    console.log('Serven on port', app.get('port'));
+});
--- src/server/config.js
+++ src/server/config.js
@@ -1,4 +1,6 @@
 module.exports = app =>{
+    //setting
+    app.set('port', process.env.PORT || 3000);
 
     return app;
 }
